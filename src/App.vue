c9923cb7aa45788e73ac7dc88771bf180ada1dc3
Julyemerson Leonizio <jb.leonizio@gmail.com>
2022-10-05 17:49:51 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen bg-myrp-dark">
+    <div class="flex flex-col  items-center h-screen">
+      <Header class="mt-20"/>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from "./components/Header.vue";
+
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex space-x-4">
+        <select class="h-12 w-96 rounded-sm bg-myrp-light p-2 text-white" name="priceTable" id="price">
+          <option value="products">Produtos</option>  
+          <option value="products">Tabela Luana Lopes</option>  
+          <option value="products">Tabela Teste</option>  
+        </select>
+        <input class="h-12 w-96 rounded-sm bg-myrp-light p-2" type="text" placeholder="CPF/CNPJ do Cliente">
+        <input class="h-12 w-96 rounded-sm bg-myrp-light p-2" type="text" placeholder="Vendedor">
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
